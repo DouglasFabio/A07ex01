71c96c6cad36b8dcb6b31d526aee798df4670afc
Douglas Fabio <douglasfabio.ifsp@gmail.com>
2023-12-08 08:47:00 -0300

    A07ex01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.a07ex01"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a07ex01"
--- app/src/main/java/com/example/a07ex01/MainActivity.kt
+++ app/src/main/java/com/example/a07ex01/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.a07ex01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edTexto = findViewById<EditText>(R.id.edTexto)
+        val btnCriar = findViewById<Button>(R.id.btnCriar)
+
+        btnCriar.setOnClickListener {
+            val texto = edTexto.text.toString()
+            mostraSnackbar(it, texto)
+
+        }
+    }
+    fun mostraSnackbar(view: View, text: String) {
+        var s = Snackbar.make(view, text, Snackbar.LENGTH_INDEFINITE)
+        s.setAction("Fechar", { s.dismiss() })
+        s.show()
     }
 }
